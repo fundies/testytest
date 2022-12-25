4968b79c4f8164767a69a1b948140e431e400f21
Greg Williamson <gwilliamson@scalecomputing.com>
2022-12-25 14:22:02 -0500

    kk

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cors = require('cors');  // import the cors package
+const { spawn } = require('child_process');
+
+// enable CORS for all routes
+app.use(cors());
+
+// serve the static files from the client directory
+app.use(express.static('client'));
+
+// listen for 'run-command' events from socket.io clients
+io.on('connection', (socket) => {
+  socket.on('run-command', (data) => {
+    const { command, args } = data;
+
+    // run the command using the child_process module
+    const child = spawn(command, args);
+
+    // listen for the 'stdout' event to receive the output of the command
+    child.stdout.on('data', (data) => {
+      // emit the output to the socket.io client
+      socket.emit('command-output', data);
+    });
+
+    // listen for the 'stderr' event to receive any errors
+    child.stderr.on('data', (data) => {
+      // emit the error to the socket.io client
+      socket.emit('command-error', data);
+    });
+
+    // listen for the 'close' event to know when the command has completed
+    child.on('close', (code) => {
+      // emit the completion event to the socket.io client
+      socket.emit('command-complete', code);
+    });
+  });
+});
+
+// start the server
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+     
+
