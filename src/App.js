33e8fca5a6c4e29070b3f891bb06922dc13b450b
Greg Williamson <gwilliamson@scalecomputing.com>
2022-12-25 13:00:39 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import CommandForm from './components/CommandForm.js';
+import OutputPanel from './components/OutputPanel.js';
 
-function App() {
+const App = () => {
+  const [output, setOutput] = useState('');  // the output of the command
+  const [error, setError] = useState('');  // any errors that occurred
+  const [isRunning, setIsRunning] = useState(false);  // whether the command is currently running
+
+  // set up a socket.io client
+  const socket = io('http://localhost:3000');
+
+  // listen for 'command-output' events and append the output to the state
+  socket.on('command-output', (data) => {
+    setOutput((prevOutput) => prevOutput + data);
+  });
+
+  // listen for 'command-error' events and update the error state
+  socket.on('command-error', (data) => {
+    setError(data);
+  });
+
+  // listen for 'command-complete' events and update the isRunning state
+  socket.on('command-complete', () => {
+    setIsRunning(false);
+  });
+
+  // send a command to the server when the form is submitted
+  const handleSubmit = (command, args) => {
+    setOutput('');  // clear the output
+    setError('');  // clear any previous errors
+    setIsRunning(true);  // set the isRunning state to true
+
+    // send the command to the server using socket.io
+    socket.emit('run-command', { command, args });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <CommandForm onSubmit={handleSubmit} isRunning={isRunning} />
+      <OutputPanel output={output} error={error} />
     </div>
   );
-}
+};
 
 export default App;
+
